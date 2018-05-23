cadb09fadd0db2521824a53bf8ca75d229d9778e
Christian Gaviria <cgaviria44@hotmail.com>
2018-05-23 14:56:12 -0500

    Update LabFinal.m

--- LabFinal.m
+++ LabFinal.m
@@ -1,15 +1,15 @@
-% Christian Camilo Gaviria Castro - 1017229318
-% Julian Castrillón García -
-% Brahian Steven Cortés -
+% Christian Camilo Gaviria Castro
+% Julian Castrillón García
+% Brahian Steven Cortés
 
 clc,clearvars,clear workspace, close all
 
 Sujeto=('a. Christian Gaviria\nb. Julian Castrillón\nc. Brahian Cortés\n');
 disp('Seleccione sujeto:')
 fprintf(Sujeto)
-subject=input(':','s');
+Usuario=input(':','s');
 
-switch subject
+switch Usuario
     case 'a' %Christian Gaviria
         Experimento=('a. Praxias\nb. Fonendoscopio\nc. Deglución\n');
         disp('Seleccione experimiento:')
@@ -25,23 +25,107 @@ switch subject
                 load('cristiangaviria-deglucion.mat')
         end
         
-        Actividad=('a. Abrir boca\nb. Apretar dientes\nc. Tirar beso\n');
-        disp('Seleccione actividad:')
-        fprintf(Actividad)
-        Gestos=input(':','s');
+    case 'b' %Julián Castrillón
+        Experimento=('a. Praxias\nb. Fonendoscopio\nc. Deglución\n');
+        disp('Seleccione experimiento:')
+        fprintf(Experimento)
+        Prueba=input(':','s');
         
-        switch Gestos
+        switch Prueba
             case 'a'
-                boca=(data(datastart(:,1):dataend(:,1)));
+                load('juliancastrillon-praxis.mat')
             case 'b'
-                dientes=(data(datastart(:,2):dataend(:,2)));
+                load('juliancastrillon-fonendoscopio.mat')
             case 'c'
-                beso=(data(datastart(:,3):dataend(:,3)));
+                load('juliancastrillon-deglucion.mat')
         end
         
-        Canal=('1. Fonendoscopio\n2. Cigomatica_R\n3. Cigomatica_L\n4. Orbicular_Sup\n5. Orbicular_Inf\n6. Suprahio_R\n7. Suprahio_L\n8. Infrahio_R\n9. Infrahio_L\n');
-        disp('Seleccione canales:')
-        fprintf(Canal)
-        Channel=input(':');
+    case 'c' %Brahian Cortés
+        Experimento=('a. Praxias\nb. Fonendoscopio\nc. Deglución\n');
+        disp('Seleccione experimiento:')
+        fprintf(Experimento)
+        Prueba=input(':','s');
         
+        switch Prueba
+            case 'a'
+                load('brahiancortes-praxis.mat')
+            case 'b'
+                load('brahiancortes-fonendoscopio.mat')
+            case 'c'
+                load('brahiancortes-deglucion.mat')
+        end
+end
+
+Actividad=('a. Abrir boca\nb. Apretar dientes\nc. Tirar beso\n');
+disp('Seleccione actividad:')
+fprintf(Actividad)
+Tarea=input(':','s');
+
+switch Tarea
+    case 'a'
+        ind=1;
+    case 'b'
+        ind=2;
+    case 'c'
+        ind=3;
+end
+
+Canal=('1. Fonendoscopio\n2. Cigomatica_R\n3. Cigomatica_L\n4. Orbicular_Sup\n5. Orbicular_Inf\n6. Suprahio_R\n7. Suprahio_L\n8. Infrahio_R\n9. Infrahio_L\n');
+disp('Seleccione canales:')
+fprintf(Canal)
+Channel=input(':');
+
+if ind==1
+    for i=1:length(Channel)
+        boca(i,:)=data(datastart(Channel(i),1):dataend(Channel(i),1));
+    end
+    
+    subplot(311)
+    plot(boca(1,:))
+    title(['El canal utilizado es: ',num2str(Channel(1))])
+    xlabel 'Tiempo [s]', ylabel 'Amplitud [V]', axis tight, grid on
+    subplot(312)
+    plot(boca(2,:))
+    title(['El canal utilizado es: ',num2str(Channel(2))])
+    xlabel 'Tiempo [s]', ylabel 'Amplitud [V]', axis tight, grid on
+    subplot(313)
+    plot(boca(3,:))
+    title(['El canal utilizado es: ',num2str(Channel(3))])
+    xlabel 'Tiempo [s]', ylabel 'Amplitud [V]', axis tight, grid on
+    
+elseif ind==2
+    for j=1:length(Channel)
+        dientes(j,:)=data(datastart(Channel(j),2):dataend(Channel(j),2));
+    end
+    
+    subplot(311)
+    plot(dientes(1,:))
+    title(['El canal utilizado es: ',num2str(Channel(1))])
+    xlabel 'Tiempo [s]', ylabel 'Amplitud [V]', axis tight, grid on
+    subplot(312)
+    plot(dientes(2,:))
+    title(['El canal utilizad es: ',num2str(Channel(2))])
+    xlabel 'Tiempo [s]', ylabel 'Amplitud [V]', axis tight, grid on
+    subplot(313)
+    plot(dientes(3,:))
+    title(['El canal utilizado es: ',num2str(Channel(3))])
+    xlabel 'Tiempo [s]', ylabel 'Amplitud [V]', axis tight, grid on
+    
+elseif ind==3
+    for k=1:length(Channel)
+        beso(k,:)=data(datastart(Channel(k),3):dataend(Channel(k),3));
+    end
+    
+    subplot(311)
+    plot(beso(1,:))
+    title(['El canal utilizado es: ',num2str(Channel(1))])
+    xlabel 'Tiempo [s]', ylabel 'Amplitud [V]', axis tight, grid on
+    subplot(312)
+    plot(beso(2,:))
+    title(['El canal utilizado es: ',num2str(Channel(2))])
+    xlabel 'Tiempo [s]', ylabel 'Ampli  tud [V]', axis tight, grid on
+    subplot(313)
+    plot(beso(3,:))
+    title(['El canal utilizado es: ',num2str(Channel(3))])
+    xlabel 'Tiempo [s]', ylabel 'Amplitud [V]', axis tight, grid on
 end
